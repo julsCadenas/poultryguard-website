b3c26569d8482a4fa6423c32cfea4610f03b6676
julsCadenas <jscadenas12@gmail.com>
2025-05-29 22:11:31 +0800

    made hero and navbar components responsive

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,20 +3,42 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <main className='flex flex-col items-center justify-center text-customWhite pt-52 gap-5'>
-        <section className='flex flex-col justify-center items-center gap-4'>
-            <p className='font-extrabold font-Inter text-customOrange text-9xl italic' >PoultryGuard</p>
-            <p className='font-semibold text-4xl' >Heat Stress Monitoring for Poultry</p>
-        </section>
-        <figure>
-            <Image
+    <main className='flex flex-col items-center justify-center text-customWhite pt-20 md:pt-52 gap-8 md:gap-15'>
+      
+      {/* title card */}
+      <section className='flex flex-col justify-center items-center gap-2 md:gap-4'>
+          <p className='font-extrabold font-Inter text-customOrange text-5xl md:text-9xl italic' >PoultryGuard</p>
+          <p className='font-semibold text-lg md:text-4xl' >Heat Stress Monitoring for Poultry</p>
+      </section>
+
+      {/* buttons */}
+      <section className='flex gap-3 md:gap-5 font-Poppins items-center justify-center'>
+          <button className='flex flex-row items-center justify-center gap-2 bg-customWhite text-customBlack p-3 rounded-2xl text-sm md:text-lg font-semibold hover:bg-customOrange transition-all'>
+            <p>View Paper</p>
+            <span className="material-symbols-outlined font-semibold text-lg">
+              code
+            </span>
+          </button>
+
+          <button className='flex flex-row items-center justify-center gap-2 bg-customWhite text-customBlack p-3 rounded-2xl text-sm md:text-lg font-semibold hover:bg-customOrange transition-all'>
+            <p>Source Code</p>
+            <span className="material-symbols-outlined">
+              description
+            </span>
+          </button>
+      </section>
+      
+      {/* image */}
+      <figure className='mx-5'>
+          <Image
             src="/images/poultryguard.png"  
             alt="Poultry"
             width={1210} 
             height={680}
-            className="rounded-lg mt-6"
-            />
-        </figure>
+            className="rounded-xl"
+          />
+      </figure>
+
     </main>
   )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,22 +1,55 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <nav className='fixed text-customWhite font-Poppins flex w-5/6 justify-between items-center px-6 py-3 m-3 rounded-3xl bg-customBlack/70 backdrop-blur'>
-        <section className='font-semibold text-3xl'>
-            <p>PoultryGuard</p>
+    <nav className='fixed text-customWhite font-Poppins w-5/6 px-6 py-3 m-3 rounded-3xl bg-customBlack/70 backdrop-blur z-50'>
+      
+      {/* title card */}
+      <div className='flex justify-between items-center'>
+        <section className='font-semibold text-2xl md:text-3xl hover:text-customOrange transition-all'>
+          <p>PoultryGuard</p>
         </section>
-        <section className='flex items-center justify-center font-semibold text-lg'>
-            <ul className='flex flex-row gap-3'>
-                <li><a href="/">Introduction</a></li>
-                <li><a href="/about">Tech Stack</a></li>
-                <li><a href="/contact">About Us</a></li>
-            </ul>
-        </section>
-        <section className='font-semibold text-xl flex flex-row gap-2 items-center justify-center'>
-          <i className="devicon-github-original text-3xl"></i>
+
+        {/* hambuyrger icon */}
+        <button
+          className='sm:hidden focus:outline-none flex items-center justify-center'
+          onClick={() => setIsOpen(!isOpen)}
+          aria-label='Toggle Menu'
+        >
+          <span className="material-symbols-outlined text-4xl">
+            {isOpen ? 'close' : 'menu'}
+          </span>
+        </button>
+
+        {/* navbar main */}
+        <div className='hidden sm:flex items-center gap-6'>
+          <ul className='flex flex-row gap-3 font-semibold text-lg'>
+            <li><a href="/" className='hover:text-customOrange transition-all'>Introduction</a></li>
+            <li><a href="/about" className='hover:text-customOrange transition-all'>Tech Stack</a></li>
+            <li><a href="/contact" className='hover:text-customOrange transition-all'>About Us</a></li>
+          </ul>
+          <section className='font-semibold text-xl flex flex-row gap-2 items-center hover:text-customOrange transition-all'>
+            <i className="devicon-github-original text-3xl"></i>
+            <p>Source</p>
+          </section>
+        </div>
+      </div>
+
+      {/* navbar mobile */}
+      <div className={`sm:hidden overflow-hidden transition-all duration-200 ease-in-out ${isOpen ? 'max-h-60 mt-4' : 'max-h-0'}`}>
+        <ul className='flex flex-col gap-3 font-semibold text-lg'>
+          <li><a href="/" className='hover:text-customOrange transition-all'>Introduction</a></li>
+          <li><a href="/about" className='hover:text-customOrange transition-all'>Tech Stack</a></li>
+          <li><a href="/contact" className='hover:text-customOrange transition-all'>About Us</a></li>
+        </ul>
+        <section className='font-semibold text-lg mt-4 flex flex-row gap-2 items-center hover:text-customOrange transition-all'>
+          <i className="devicon-github-original text-xl"></i>
           <p>Source</p>
         </section>
+      </div>
     </nav>
   )
 }
